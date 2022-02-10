fceaa4f9f7b3fe3c533c4c5ea1e2040d1447c221
Joao Vitor Lima Lago Santos <vorque45@gmail.com>
2022-02-09 22:02:05 -0300

    adiçao da rota de redefiniçao de senha

--- src/modules/users/routes/password.routes.ts
+++ src/modules/users/routes/password.routes.ts
@@ -1,9 +1,11 @@
 import { celebrate, Joi, Segments } from 'celebrate'
 import { Router } from 'express'
 import ForgotPasswordController from '../Controllers/ForgotPasswordController'
+import ResetPasswordController from '../Controllers/ResetPasswordController'
 
 const passwordRouter = Router()
 const forgotPasswordController = new ForgotPasswordController()
+const resetPasswordController = new ResetPasswordController()
 
 passwordRouter.post('/forgot',
 	celebrate({
@@ -12,5 +14,16 @@ passwordRouter.post('/forgot',
 		}
 	}),
 	forgotPasswordController.create)
+
+passwordRouter.post('/reset',
+	celebrate({
+		[Segments.BODY]:{
+			token: Joi.string().uuid().required(),
+			password: Joi.string().required(),
+			password_confirmation: Joi.string().required().valid(Joi.ref('password'))
+		}
+	}),
+	resetPasswordController.create
+)
 
 export default passwordRouter
