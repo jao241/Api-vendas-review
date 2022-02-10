b73900b9f6014205ea55de22dca8f686e0d3593d
Joao Vitor Lima Lago Santos <vorque45@gmail.com>
2022-02-09 22:00:58 -0300

    adiçao do envio do fake email

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -2,6 +2,7 @@ import AppError from '@shared/errors/AppError'
 import { getCustomRepository } from 'typeorm'
 import UsersRepository from '../typeorm/repositories/UsersRepository'
 import UserTokensRepository from '../typeorm/repositories/UserTokensRepository'
+import EtherealMail from '@config/mail/EtherealMail'
 
 interface IRequest{
     email: string
@@ -19,7 +20,9 @@ export default class SendForgotPasswordEmailService{
 
 		const token = await userTokenRepository.generate(user.id)
 
-		console.log(token)
-
+		await EtherealMail.sendMail({
+			to: email,
+			body: `Solicitação de redefinição de senha recebida: ${token?.token}`
+		})
 	}
 }
